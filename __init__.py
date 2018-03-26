6fca698601b524c35de8344413c88adef8cfabd7
Florent Martin <florent.guy.martin@gmail.com>
2018-03-26 19:36:00 +0200

    remove .aux files

--- __init__.py
+++
@@ -1,6 +0,0 @@
-#from Py_Data_talk.src.data.generate_dataset import *
-#print('ok')
-import Py_Data_talk.src
-import Py_Data_talk.models
-from Py_Data_talk.src.data import *
-from  Py_Data_talk.src.data import generate_data 
